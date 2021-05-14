33e201880c5b52957859c5f49bc7d1b39930306a
Ndvuong2302 <nguyenducvuong2302@gmail.com>
2021-05-14 16:45:52 +0700

    update dialog.

--- src/components/dialog/DialogInput.js
+++ src/components/dialog/DialogInput.js
@@ -43,14 +43,14 @@ export default (props) => {
     }
     const renderItem = (item, index) => {
         return (
-            <View style={{paddingHorizontal:Metrics.screenWidth*0.07}} >
+            <View style={{paddingHorizontal:Metrics.screenWidth*0.03}} >
                 <Text style={styles.styleContent}>{I18n.t(item.Name)}</Text>
                 <TextInput style={styles.styleTextInput} keyboardType={typeof(item.Value) == 'number' ? 'numbers-and-punctuation' : 'default'} value={item.Value ? typeof (item.Value) == 'string' ? item.Value : currencyToString(item.Value) : null} placeholder={I18n.t(item.Hint)} placeholderTextColor="#808080" onChangeText={(text) => { setInput(item.isNum == true ? onChangeTextInput(text):text); item.Value = item.isNum== true? onChangeTextInput(text) : text ; setKey(item.Key); setList([...list]); }}></TextInput>
             </View>
         )
     }
     return (
-        <View style={{ backgroundColor: 'white', borderRadius: 5 }}>
+        <View style={{ backgroundColor: 'white', borderRadius: 5, width:Metrics.screenWidth*0.65, marginLeft:Metrics.screenWidth*0.075 }}>
             <Text style={[styles.styleTitle, { marginTop: 10 }]}>{props.title}</Text>
             <View style={styles.styleLine}></View>
                 <FlatList
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
     },
     styleButton: {
         backgroundColor: colors.colorLightBlue,
-        borderRadius: 10,  marginBottom: 20,marginHorizontal:Metrics.screenWidth*0.07+10
+        borderRadius: 10,  marginBottom: 20,marginHorizontal:Metrics.screenWidth*0.03+10
     },
     styleTextInput: {
         borderRadius: 10, padding: 12, marginRight: 10, marginLeft: 10, fontSize: 14, color: "#4a4a4a",backgroundColor:'#f2f2f2', borderColor:'#bbbbbb',borderWidth:0.5
