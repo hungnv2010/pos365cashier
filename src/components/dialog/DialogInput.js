a07a717a73dd258f3f6c326f6f45d08d69845bbb
Ndvuong2302 <nguyenducvuong2302@gmail.com>
2021-05-28 09:32:56 +0700

    update.

--- src/components/dialog/DialogInput.js
+++ src/components/dialog/DialogInput.js
@@ -17,7 +17,7 @@ export default (props) => {
 
     const onClickButton = () => {
         props.outputValue(value)
-        console.log("value",JSON.stringify(value));
+        console.log("value", JSON.stringify(value));
 
 
     }
@@ -30,7 +30,7 @@ export default (props) => {
         list.forEach(element => {
             value[element.Key] = element.Value
         });
-        console.log("object",JSON.stringify(value));
+        console.log("object", JSON.stringify(value));
     }, [props.listItem])
     useEffect(() => {
         let obj = value
@@ -50,21 +50,21 @@ export default (props) => {
     }
     const renderItem = (item, index) => {
         return (
-            <View style={{paddingHorizontal:Metrics.screenWidth*0.03}} >
+            <View style={{ paddingHorizontal: Metrics.screenWidth * 0.03 }} >
                 <Text style={styles.styleContent}>{I18n.t(item.Name)}</Text>
-                <TextInput style={styles.styleTextInput} keyboardType={item.isNum ? item.isNum : 'default'} value={item.Value ? typeof (item.Value) == 'string' ? item.Value : currencyToString(item.Value) : null} placeholder={item.Hint != '' ? I18n.t(item.Hint) : item.Hint} placeholderTextColor="#bbbbbb" onChangeText={(text) => { setInput(item.isNum == true ? onChangeTextInput(text):text); item.Value = item.isNum== true? onChangeTextInput(text) : text ; setKey(item.Key); setList([...list]); }}></TextInput>
+                <TextInput style={styles.styleTextInput} keyboardType={item.isNum ? item.isNum : 'default'} value={item.Value ? item.isNum ? currencyToString(item.Value) : item.Value : null} placeholder={item.Hint != '' ? I18n.t(item.Hint) : item.Hint} placeholderTextColor="#bbbbbb" onChangeText={(text) => { setInput(item.isNum ? onChangeTextInput(text) : text); item.Value = item.isNum ? onChangeTextInput(text) : text; setKey(item.Key); setList([...list]); }}></TextInput>
             </View>
         )
     }
     return (
-        <View style={{ backgroundColor: 'white', borderRadius: 5, width:deviceType == Constant.PHONE ? Metrics.screenWidth*0.8 : Metrics.screenWidth*0.65, marginLeft:deviceType == Constant.TABLET ? Metrics.screenWidth*0.075 :0 }}>
+        <View style={{ backgroundColor: 'white', borderRadius: 5, width: deviceType == Constant.PHONE ? Metrics.screenWidth * 0.8 : Metrics.screenWidth * 0.65, marginLeft: deviceType == Constant.TABLET ? Metrics.screenWidth * 0.075 : 0 }}>
             <Text style={[styles.styleTitle, { marginTop: 10 }]}>{props.title}</Text>
             <View style={styles.styleLine}></View>
-                <FlatList
-                    data={list}
-                    renderItem={({ item, index }) => renderItem(item, index)}
-                    keyExtractor={(item, index) => index.toString()}
-                />
+            <FlatList
+                data={list}
+                renderItem={({ item, index }) => renderItem(item, index)}
+                keyExtractor={(item, index) => index.toString()}
+            />
             <Text style={[styles.styleContent]}>{props.content}</Text>
             <TouchableOpacity style={styles.styleButton} onPress={onClickButton}>
                 <Text style={[styles.styleTitle, { color: 'white' }]}>{props.titleButton}</Text>
@@ -82,10 +82,10 @@ const styles = StyleSheet.create({
     },
     styleButton: {
         backgroundColor: colors.colorLightBlue,
-        borderRadius: 10,  marginBottom: 20,marginHorizontal:Metrics.screenWidth*0.03+10
+        borderRadius: 10, marginBottom: 20, marginHorizontal: Metrics.screenWidth * 0.03 + 10
     },
     styleTextInput: {
-        borderRadius: 10, padding: 12, marginRight: 10, marginLeft: 10, fontSize: 14, color: "#4a4a4a",backgroundColor:'#f2f2f2', borderColor:'#bbbbbb',borderWidth:0.5
+        borderRadius: 10, padding: 12, marginRight: 10, marginLeft: 10, fontSize: 14, color: "#4a4a4a", backgroundColor: '#f2f2f2', borderColor: '#bbbbbb', borderWidth: 0.5
     },
     styleLine: {
         height: 1, marginLeft: 10, marginRight: 10, backgroundColor: '#DCDCDC'
